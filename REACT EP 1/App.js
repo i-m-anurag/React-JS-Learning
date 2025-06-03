af2dc21c5e47cd4980a4bc23c0e7f670ad9e8f3e
i-m-anurag <iamanurag74@gmail.com>
2025-06-03 12:31:12 +0530

    Intial Commit

---
+++ REACT EP 1/App.js
@@ -0,0 +1,15 @@
+const div = React.createElement("div", { id: "div1" },
+    React.createElement(
+        "div",
+        { id: 'div 2' },
+        [React.createElement("h1", {}, "I am Heading"),
+        React.createElement("h2", {}, "I am Heading 2")
+        ]
+    )
+)
+
+
+const heading = React.createElement("h1", { id: 'heading' }, "Hello world !!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(div)
+root.render(div);
